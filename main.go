6fa504da285178017a8eebfa0c7b023079a62112
Aksh <aj225patel@gmail.com>
2024-05-12 18:59:29 +0530

    Installed dependancies and resolved issues

--- main.go
+++ main.go
@@ -1,34 +1,36 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/aksh-patel1/go-ecommerce-platform/controllers"
 	"github.com/aksh-patel1/go-ecommerce-platform/database"
 	"github.com/aksh-patel1/go-ecommerce-platform/middleware"
 	"github.com/aksh-patel1/go-ecommerce-platform/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		port = "8000"
 	}
-
 	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
 
 	router := gin.New()
 	router.Use(gin.Logger())
-
-	router.UserRoutes(router)
+	routes.UserRoutes(router)
 	router.Use(middleware.Authentication())
-
 	router.GET("/addtocart", app.AddToCart())
-	router.GET("removeitem", app.RemoveItem())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
 	router.GET("/cartcheckout", app.BuyFromCart())
 	router.GET("/instantbuy", app.InstantBuy())
-
 	log.Fatal(router.Run(":" + port))
 }
